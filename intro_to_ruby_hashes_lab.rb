d9be5cd73b1a98b2cf2e27da09883d8228cd35a3
Whittney Davis <wcdavis89@yahoo.com>
2019-11-04 03:09:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,10 @@ def monopoly_with_fourth_tier
    :rent_in_dollars =>{
      :one_piece_owned =>25, :two_pieces_owned =>50, :three_pieces_owned =>100, :four_pieces_owned =>200}
    :names =>{
-     :reading_railroad
-     :pennsylvania_railroad
-     :
+     :reading_railroad =>{}
+     :pennsylvania_railroad =>{}
+     :b_and_o_railroad =>{}
+     :shortline_railroad =>{}
    }
  }
 }
