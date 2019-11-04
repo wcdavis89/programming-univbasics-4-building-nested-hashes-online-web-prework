779fcc2c2779c11afa1188eaec9fde33624d1b3f
Whittney Davis <wcdavis89@yahoo.com>
2019-11-04 03:02:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  base_hash={:railroads =>{:pieces =>4, :rent_in_dollars =>{:one_piece_owned =>25, :two_pieces_owned =>50, :three_pieces_owned =>100, }}
+  base_hash={:railroads =>{:pieces =>4, :rent_in_dollars =>{:one_piece_owned =>25, :two_pieces_owned =>50, :three_pieces_owned =>100, :four_pieces_owned =>200}}
 }
 end
 
